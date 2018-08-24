05ccde1380ff0202eb1f2e1b818f0e32ff8c9fbb
hclimente <hclimente@gmail.com>
2018-08-24 13:34:21 +0900

    added prototype for imputation pipeline

---
+++ bin/templates/io/chunk_legend.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# LEGEND, CHUNK_SIZE
+# chunks
+import gzip
+
+chromosome = '${LEGEND}'.split('.')[0].replace('1000GP_Phase3_chr', '')
+first_pos = 0
+last_pos = float('-inf')
+
+with gzip.open('${LEGEND}', mode = 'rt') as legend:
+  legend.readline()
+  for line in legend.readlines():
+    line = line.strip().split(' ')
+    pos = int(line[1])
+    if pos < first_pos: first_pos = pos
+    if pos > last_pos: last_pos = pos
+
+with open('chunks', 'w') as chunks:
+  for i in range(first_pos, last_pos, ${CHUNK_SIZE}):
+    chunks.write('{}\\t{}\\t{}\\n'.format(chromosome, i, min(i + ${CHUNK_SIZE}, last_pos)))
---
+++ bin/templates/io/ped2gen.sh
@@ -0,0 +1,4 @@
+# PED, MAP
+# out.sample, out.gen
+
+gtool -P --ped ${PED} --map ${MAP} --og out.gen --os out.sample --binary_phenotype --family
