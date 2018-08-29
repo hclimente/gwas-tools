7fe241162b364fc542215d40301d51bcf185d108
hclimente <hclimente@gmail.com>
2018-08-29 13:32:55 +0900

    bugfixes

--- bin/templates/impute_chunk.sh
+++ bin/templates/impute_chunk.sh
@@ -15,14 +15,14 @@ fi
 ## only genotyped snps are included in the panel
 ## use 503 samples for the reference with european origin
 impute2 \
-    -g ${GEN} \
-    -m ${REFERENCE}/genetic_map_chr${CHR}_combined_b37.txt \
-    -int ${START} ${END} \
-    -h ${REFERENCE}/1000GP_Phase3_chr${CHR}.hap.gz \
-    -l ${REFERENCE}/1000GP_Phase3_chr${CHR}.legend.gz \
-    -Ne 20000 \
-    -verbose \
-    \$chrXflags \
-    -k_hap 503 \
-    -os 2 -o imputed.gen \
-    #-strand_g \${STRAND_INFO} \
+-g ${GEN} \
+-m ${REFERENCE}/genetic_map_chr${CHR}_combined_b37.txt \
+-int ${START} ${END} \
+-h ${REFERENCE}/1000GP_Phase3_chr${CHR}.hap.gz \
+-l ${REFERENCE}/1000GP_Phase3_chr${CHR}.legend.gz \
+-Ne 20000 \
+-verbose \
+\$chrXflags \
+-k_hap 503 \
+-os 2 -o imputed.gen \
+#-strand_g \${STRAND_INFO} \
