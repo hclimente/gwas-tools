af47483547eee5da2dd605925eb4e6b9ce79d20a
hclimente <hclimente@gmail.com>
2019-02-19 14:17:21 +0100

    add genename to leanr output

--- bin/templates/discovery/run_leanr.R
+++ bin/templates/discovery/run_leanr.R
@@ -10,4 +10,7 @@ scores <- scores[names(scores) %in% names(V(net))]
 results <- run.lean(scores, net, n_reps = 10000, 
                     add.scored.genes = TRUE, verbose = TRUE)
 
-write_tsv(as.data.frame(results\$restab), 'scored_genes.lean.txt')
+as_tibble(results\$restab) %>%
+    mutate(Gene = rownames(results\$restab)) %>%
+    select(Gene, everything()) %>%
+    write_tsv('scored_genes.lean.txt')
