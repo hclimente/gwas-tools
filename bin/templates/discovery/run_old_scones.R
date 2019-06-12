e7a91d555a3a49ab098bc3f8cfcd473f1c5d4e02
hclimente <hclimente@gmail.com>
2019-06-12 21:12:22 +0200

    bugfixes

--- bin/templates/discovery/run_old_scones.R
+++ bin/templates/discovery/run_old_scones.R
@@ -9,7 +9,7 @@ load("$RNET")
 params <- capture.output(
   cones <- search_cones(gwas, net,
                         associationScore = "${SCORE}",
-                        modelScore = "${}CRITERION}")
+                        modelScore = "${CRITERION}")
 ) %>% tail(n = 2) %>% lapply(strsplit, ' = ') %>% unlist %>% .[c(F,T)] %>% as.numeric() %>% log10
 
 etas <- 10^seq(params[1] - 1.5, params[1] + 1.5, length.out = 10)
