db8dd30f5aac46164bcf808b02e9a11bcabda25b
hclimente <hclimente@gmail.com>
2020-05-14 00:32:51 +0200

    output are tab files

--- bin/one_hot_bed.sh
+++ bin/one_hot_bed.sh
@@ -1,4 +1,6 @@
 plink -bfile ${1} -recode oxford
 
-cut -d' ' -f2,6- plink.gen >X.dat
-cut -d' ' -f5 plink.sample >y.dat
+cut -d' ' -f2,6- plink.gen | sed 's/ /\t/g' >X.tsv
+cut -d' ' -f5 plink.sample >y.tsv
+
+rm plink.gen plink.sample
