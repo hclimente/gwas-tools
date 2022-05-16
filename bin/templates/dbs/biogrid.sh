1058171a835009513ed234333cb4f729bd741cc1
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2022-05-16 18:08:27 +0200

    Correcting typo

--- bin/templates/dbs/biogrid.sh
+++ bin/templates/dbs/biogrid.sh
@@ -3,5 +3,5 @@ wget https://downloads.thebiogrid.org/Download/BioGRID/Latest-Release/BIOGRID-MV
 unzip BIOGRID-MV-Physical-LATEST.tab2.zip
 mv *.tab2.txt tab2
 # if not at root of gwas-tools directory, change path so it gets into gwas-tools/test/data/
-mv tab2 /test/data/
+mv tab2 test/data/
 rm BIOGRID-MV-Physical-LATEST.tab2.zip
