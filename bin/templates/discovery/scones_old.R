dd733ce78576bd671d2427d13606be69e0aa655a
Héctor Climente-González <hclimente@gmail.com>
2022-08-03 14:11:12 +0900

    fixes

--- bin/templates/discovery/scones_old.R
+++ bin/templates/discovery/scones_old.R
@@ -15,15 +15,15 @@ params <- capture.output(
 
 etas <- 10^seq(params[1] - 1.5, params[1] + 1.5, length.out = 10)
 lambdas <- 10^seq(params[2] - 1.5, params[2] + 1.5, length.out = 10)
-subnet <- scones.cv(gwas, net,
-                      score = "${SCORE}",
-                      criterion = "${CRITERION}",
-                      etas = etas,
-                      lambdas = lambdas)
+subnet <- scones.cv(gwas, 
+                    net,
+                    score = "${SCORE}",
+                    criterion = "${CRITERION}",
+                    etas = etas,
+                    lambdas = lambdas)
 
 cones <- martini:::get_snp_modules(gwas, subnet)
 cones[['c']] <- martini:::snp_test(gwas, covars, "${SCORE}")
 cones[['selected']] <- cones[['snp']] %in% names(V(subnet))
-
 
 write_tsv(cones, 'cones.tsv')
